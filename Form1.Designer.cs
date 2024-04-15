4f5f05d86e997ecc04a54c35649622e4c2515afc
Yorick <y.kovanev@mail.ru>
2024-04-15 10:24:26 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace lab_prog_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(436, 237);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_prog_4
+{
+    public partial class Form1 : Form
+    {
+        int currentPlayer;
+        int fieldSize;
+        int columnSize;
+        int rowSize;
+
+        int[] scoresX = new int[8];
+        int[] scoresO = new int[8];
+
+        public Form1()
+        {
+            InitializeComponent();
+            currentPlayer = 0;
+            fieldSize = 3;
+
+            dataGridView1.ColumnCount = fieldSize;
+            dataGridView1.RowCount = fieldSize;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                foreach (DataGridViewCell cell in row.Cells)
+                    cell.Value = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.ColumnIndex;
+            int j = e.RowIndex;
+
+            string selectedCell = dataGridView1[i, j].Value.ToString();
+
+            if (selectedCell != "")
+                return;
+
+            if (currentPlayer == 0) {
+                scoresX[i]++;
+                scoresX[j + 3]++;
+                if (i == j)
+                    scoresX[6]++;
+                if (i + j == 2)
+                    scoresX[7]++;
+                selectedCell = "╳";
+            }
+            if (currentPlayer == 1) {
+                scoresO[i]++;
+                scoresO[j + 3]++;
+                if (i == j)
+                    scoresO[6]++;
+                if (i + j == 2)
+                    scoresO[7]++;
+                selectedCell = "◯";
+            }
+
+            dataGridView1[i,j].Value = selectedCell;
+
+            currentPlayer += 1;
+            currentPlayer %= 2;
+
+            if (scoresX.Contains(3))
+                MessageBox.Show("╳ wins");
+            if (scoresO.Contains(3))
+                MessageBox.Show("◯ wins");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+            columnSize = dataGridView1.Width / fieldSize;
+            rowSize = dataGridView1.Height / fieldSize;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                row.Height = rowSize;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+                column.Width = columnSize;
+        }
+    }
+}
